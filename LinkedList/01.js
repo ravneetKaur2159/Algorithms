55b6ae4f5ee968375dc29157e5a36d76da20e6e9
Ravneet Kaur <47275109+ravneetKaur2159@users.noreply.github.com>
2019-02-26 16:08:45 +0800

    Addition in a linked list

--- LinkedList/01.js
+++ LinkedList/01.js
@@ -30,3 +30,15 @@ newList.addHeadNode(100);
 newList.addHeadNode(200);
 newList.addNodesAtTailEnd(50);
 newList.addHeadNode(300);
+
+// Add a node at the end of the list without using tail pointer
+LinkedList.prototype.addNodeAtEndNoTailPointer = function(value){
+    var newNode = new Node(value, null);
+    var node = this.head;
+    while(node.next!=null){
+        node=node.next;
+    }
+    node.next = newNode;
+}
+
+newList.addNodeAtEndNoTailPointer(40);
