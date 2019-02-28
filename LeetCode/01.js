4ca5622beeced49fc7c93b72d8c868a3b1da0a25
Ravneet Kaur <47275109+ravneetKaur2159@users.noreply.github.com>
2019-02-28 23:46:46 +0800

    add 2 numbers with recursive approach

--- LeetCode/01.js
+++ LeetCode/01.js
@@ -20,6 +20,46 @@ function sumTwoNumbers(){
     }
     console.log("There are no two indices that add up to the given sum");
 }
-sumTwoNumbers();
+//sumTwoNumbers();
 
 
+// Sort array in new array and find indices in 1 loop - O(nLogn + n) --> O(nLogn)
+// Bubble sort
+var newArray = givenArray;
+
+function recursiveSort(startIndex, endIndex){
+    if(typeof startIndex != "undefined" && startIndex == endIndex){
+        return startIndex;
+    }
+
+    if(startIndex != endIndex){
+        var middleLen = Math.floor((startIndex+endIndex)/2);
+        var firstElementIndex = recursiveSort(startIndex,middleLen);
+        var secondElementIndex = recursiveSort(middleLen+1, endIndex);
+        return newArray[firstElementIndex] >= newArray[secondElementIndex] ? firstElementIndex : secondElementIndex;
+    }
+
+    for(var i=0;i<newArray.length;i++){
+        var maxElementIndex = recursiveSort(0,newArray.length-(1+i));
+        var temp = newArray[newArray.length-(1+i)];
+        newArray[newArray.length-(1+i)] = newArray[maxElementIndex];
+        newArray[maxElementIndex] = temp;
+    }
+}
+
+recursiveSort();
+maxElementSortedArray();
+//Finding Max element given array is sorted
+function maxElementSortedArray() {
+    for(var i=0,j = newArray.length-1;i<j;){
+        if(newArray[i]+newArray[j] < sum){
+            i++;
+        }else if(newArray[i]+newArray[j] > sum){
+            j--;
+        }else{
+            console.log("The two indices are : "+i+" and "+j);
+            return;
+        }
+    }
+    console.log("There are no two indices that add up to the given sum");
+}
